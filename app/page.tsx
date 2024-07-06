f9a3bfe23f3d460b5650fa70a1e8ce84699c0e3f
AlexandreDresch <connectWithAlexandre@proton.me>
2024-07-05 23:51:43 -0300

    feat: add Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,41 @@
+import UserForm from "@/components/forms/user-form";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="Logo"
+            className="h10 mb-12 w-fit"
+          />
+
+          <UserForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 FixMyCar
+            </p>
+
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/onboarding-img.jpg"
+        height={1000}
+        width={1000}
+        alt="Mechanic"
+        className="side-img max-w-[50%]"
+      />
+    </div>
   );
 }
