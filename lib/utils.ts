0d39de938b276b5e4c0697a2f539c8943862649f
AlexandreDresch <connectWithAlexandre@proton.me>
2024-07-06 23:50:35 -0300

    feat: add nameRegex & phoneRegex to utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,9 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const nameRegex = /^[a-zA-ZÀ-ÖØ-öø-ÿ' -]+$/;
+export const phoneRegex = /^\(?\d{2,4}\)?[-.\s]?\d{3,4}[-.\s]?\d{3,4}$/;
