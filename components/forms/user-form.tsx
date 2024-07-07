57fc37fc708262ddf981b48dd440d22572408a62
AlexandreDresch <connectWithAlexandre@proton.me>
2024-07-06 23:52:53 -0300

    feat: add UserForm component

---
+++ components/forms/user-form.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { userSchema } from "@/schemas/user-schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Form } from "@/components/ui/form";
+import { z } from "zod";
+import CustomFormField from "../custom-form-field";
+import SubmitButton from "../submit-button";
+import { useState } from "react";
+
+export enum FormFieldType {
+  INPUT = "input",
+  SELECT = "select",
+  TEXTAREA = "textarea",
+  CHECKBOX = "checkbox",
+  RADIO = "radio",
+  PHONE_INPUT = "phoneInput",
+  DATE_PICKER = "datePicker",
+  SKELETON = "skeleton",
+}
+
+export default function UserForm() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof userSchema>>({
+    resolver: zodResolver(userSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof userSchema>) {
+    setIsLoading(true);
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-6">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there</h1>
+          <p className="text-dark-700">Schedule your first visit.</p>
+        </section>
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldType.INPUT}
+          label="Full Name"
+          name="name"
+          placeholder="Enter your full name"
+          icon="./assets/icons/user.svg"
+          iconAlt="Person Icon"
+        />
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldType.INPUT}
+          label="Email"
+          name="email"
+          placeholder="Enter your email address"
+          icon="./assets/icons/email.svg"
+          iconAlt="Email Icon"
+        />
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldType.PHONE_INPUT}
+          label="Phone Number"
+          name="phone"
+          placeholder="Phone Number"
+        />
+
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+      </form>
+    </Form>
+  );
+}
